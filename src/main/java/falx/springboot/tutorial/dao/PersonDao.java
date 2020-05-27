726079bb75c8fd77b9c5d940baaaec12755fc6e4
blaise-m <mubangiziblaise@gmail.com>
2020-05-27 17:10:23 +0300

    Initial Commit

---
+++ src/main/java/falx/springboot/tutorial/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package falx.springboot.tutorial.dao;
+
+import falx.springboot.tutorial.model.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> getAllPeople();
+}
